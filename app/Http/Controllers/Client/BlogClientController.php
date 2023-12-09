97075f8085091fd6072e5a5bed5a8f8c63d955ff
uQHan <hanng.22ite@vku.udn.vn>
2023-12-09 11:55:04 +0700

    category/comment numbers and recent/random blogs

--- app/Http/Controllers/Client/BlogClientController.php
+++ app/Http/Controllers/Client/BlogClientController.php
@@ -13,10 +13,21 @@ class BlogClientController extends Controller
     public function index()
     {
         $blogs = DB::table('blogs')
-            ->join('users', 'blogs.user_id', '=', 'users.user_id')
-            ->select('blogs.blog_id','blogs.title','blogs.text','blogs.image_url','blogs.created_at','users.user_id','users.username')
+            ->leftjoin('blog_comments', 'blogs.blog_id', '=', 'blog_comments.blog_id')
+            ->select('blogs.blog_id', 'blogs.title', 'blogs.text', 'blogs.image_url', 'blogs.created_at', DB::raw('count(blog_comments.blog_id) as com_count'))
+            ->groupBy('blogs.blog_id', 'blogs.title', 'blogs.text', 'blogs.image_url', 'blogs.created_at')
+            ->orderBy('blogs.created_at', 'desc')
             ->paginate(5);
-        return view("client.blog", compact('blogs'));
+        $category_count = DB::table('categories')
+            ->join('products', 'categories.category_id', '=', 'products.category_id')
+            ->select('categories.category_id', 'categories.name', DB::raw('count(*) as total'))
+            ->groupBy('category_id', 'name')
+            ->get();
+        $randoms = DB::table('blogs')
+            ->inRandomOrder()
+            ->take(3)
+            ->get();
+        return view("client.blog", compact('blogs', 'category_count', 'randoms'));
     }
 
     public function blog_single_model()
@@ -28,22 +39,25 @@ class BlogClientController extends Controller
     {
 
         $blog = DB::table('blogs')
-        ->where('blog_id', $blog_id)
-        ->join('users', 'blogs.user_id', '=', 'users.user_id')
-        ->select('blogs.blog_id','blogs.title','blogs.text','blogs.image_url','blogs.created_at','users.user_id','users.username')
-        ->first();
-        // $comments = DB::table('blog_comments')
-        // ->join('users', 'blog_comments.user_id', '=', 'users.user_id')
-        // ->select('blog_comments.comment_id','blog_comments.unreg_username','blog_comments.text','blog_comments.image_url','blog_comments.created_at','users.user_id','users.username')
-        // ->orderBy('blog_comments.created_at','desc')
-        // ->get();
+            ->where('blog_id', $blog_id)
+            ->select('blogs.blog_id', 'blogs.title', 'blogs.text', 'blogs.image_url', 'blogs.created_at')
+            ->first();
         $comments = DB::table('blog_comments')
-        ->where('blog_id', $blog_id)
-        ->leftjoin('users', 'blog_comments.user_id', '=', 'users.user_id')
-        ->select('blog_comments.comment_id','blog_comments.unreg_username','blog_comments.text','blog_comments.image_url','blog_comments.created_at','users.user_id','users.username')
-        ->orderBy('blog_comments.created_at','asc')
-        ->get();
-        return view("client.blog-singles", compact("blog","comments"));
+            ->where('blog_id', $blog_id)
+            ->leftjoin('users', 'blog_comments.user_id', '=', 'users.user_id')
+            ->select('blog_comments.*', 'users.user_id', 'users.username')
+            ->orderBy('blog_comments.created_at', 'asc')
+            ->get();
+        $category_count = DB::table('categories')
+            ->join('products', 'categories.category_id', '=', 'products.category_id')
+            ->select('categories.category_id', 'categories.name', DB::raw('count(*) as total'))
+            ->groupBy('category_id', 'name')
+            ->get();
+        $recents = DB::table('blogs')
+            ->where('blog_id', '!=', $blog_id)
+            ->orderBy('blogs.created_at', 'desc')
+            ->paginate(3);
+        return view("client.blog-singles", compact("blog", "comments", "category_count", "recents"));
     }
     public function store(Request $request)
     {
